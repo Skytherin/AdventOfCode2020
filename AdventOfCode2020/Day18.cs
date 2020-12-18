e1572072e58d4b8b73edb2c586c368060373b630
Jake Baker <jbaker@zotecpartners.com>
2020-12-18 12:21:42 -0500

    Day18 bit of cleanup

--- AdventOfCode2020/Day18.cs
+++ AdventOfCode2020/Day18.cs
@@ -13,49 +13,61 @@ namespace AdventOfCode2020
         {
             var input = ConvertInput(File.ReadAllText("Inputs/Day18.txt"));
             var timer = new MyTimer();
-            Run(ConvertInput("2 * 3 + (4 * 5)")).Should().Be(26);
-            Run(ConvertInput("5 + (8 * 3 + 9 + 3 * 4 * 3)")).Should().Be(437);
-            Run(ConvertInput("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).Should().Be(12240);
-            Run(ConvertInput("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).Should().Be(13632);
-            Run(input).Should().Be(464478013511L); 
+            var expression = new MathRdp();
+            Run(ConvertInput("2 * 3 + (4 * 5)"), expression).Should().Be(26);
+            Run(ConvertInput("5 + (8 * 3 + 9 + 3 * 4 * 3)"), expression).Should().Be(437);
+            Run(ConvertInput("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), expression).Should().Be(12240);
+            Run(ConvertInput("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), expression).Should().Be(13632);
+            Run(input, expression).Should().Be(464478013511L); 
 
             timer.Lap();
-
-            Run2(ConvertInput("1 + (2 * 3) + (4 * (5 + 6))")).Should().Be(51);
-            Run2(ConvertInput("2 * 3 + (4 * 5)")).Should().Be(46);
-            Run2(ConvertInput("5 + (8 * 3 + 9 + 3 * 4 * 3)")).Should().Be(1445);
-            Run2(ConvertInput("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).Should().Be(669060);
-            Run2(ConvertInput("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).Should().Be(23340);
-            Run2(input).Should().Be(85660197232452L);
+            expression = new MathRdpWithPrecedence();
+            Run(ConvertInput("1 + (2 * 3) + (4 * (5 + 6))"), expression).Should().Be(51);
+            Run(ConvertInput("2 * 3 + (4 * 5)"), expression).Should().Be(46);
+            Run(ConvertInput("5 + (8 * 3 + 9 + 3 * 4 * 3)"), expression).Should().Be(1445);
+            Run(ConvertInput("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"), expression).Should().Be(669060);
+            Run(ConvertInput("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"), expression).Should().Be(23340);
+            Run(input, expression).Should().Be(85660197232452L);
 
             timer.Lap();
             timer.Total();
         }
 
-        private static long Run(List<List<MathElement>> state)
+        private static long Run(IEnumerable<List<MathToken>> state, MathRdp expression)
         {
-            return state.Sum(line => DoMath(new Queue<MathElement>(line), false));
+            return state.Sum(line => expression.Expression(new Queue<MathToken>(line)));
         }
 
-        private static long Run2(List<List<MathElement>> state)
+        private static List<List<MathToken>> ConvertInput(string input)
         {
-            return state.Sum(line => DoMath(new Queue<MathElement>(line), true));
+            return input.SplitIntoLines()
+                .Select(line => RegexUtils.DeserializeMany<MathToken>(line, @"(?<Number>\d+)|(?<Operator>[+*()])"))
+                .ToList();
         }
+    }
 
-        private static long DoMath(Queue<MathElement> line, bool useOpPrecedence)
+    public class MathToken
+    {
+        public long? Number { get; set; }
+        public char? Operator { get; set; }
+    }
+
+    public class MathRdp
+    {
+        public long Expression(Queue<MathToken> line)
         {
-            var first = ReadOneTerm(line, useOpPrecedence);
+            var first = Term(line);
             while (line.Any())
             {
                 var op = line.Dequeue();
-                var second = ReadOneTerm(line, useOpPrecedence);
+                var second = Term(line);
                 if (op.Operator == '+')
                 {
-                    first = first + second;
+                    first += second;
                 }
                 else if (op.Operator == '*')
                 {
-                    first = first * second;
+                    first *= second;
                 }
                 else throw new ApplicationException();
             }
@@ -63,15 +75,17 @@ namespace AdventOfCode2020
             return first;
         }
 
-        private static long ReadOneTerm(Queue<MathElement> line, bool useOpPrecedence)
+        protected virtual long Term(Queue<MathToken> line) => BaseTerm(line);
+
+        protected long BaseTerm(Queue<MathToken> line)
         {
             var first = line.Dequeue();
-            var result = 0L;
-            if (first.Number is {} n) result = n;
-            else if (first.Operator == '(')
+            if (first.Number is { } n) return n;
+
+            if (first.Operator == '(')
             {
                 var count = 1;
-                var subTerm = new Queue<MathElement>();
+                var subTerm = new Queue<MathToken>();
                 while (true)
                 {
                     var next = line.Dequeue();
@@ -80,31 +94,27 @@ namespace AdventOfCode2020
                     if (next.Operator == ')') count--;
                     if (next.Operator == '(') count++;
                 }
-                result = DoMath(subTerm, useOpPrecedence);
+                return Expression(subTerm);
             }
-            else throw new ApplicationException();
 
-            if (useOpPrecedence && line.Count > 0 && line.Peek().Operator == '+')
+            throw new ApplicationException();
+        }
+    }
+
+    public class MathRdpWithPrecedence: MathRdp
+    {
+        protected override long Term(Queue<MathToken> line)
+        {
+            var result = BaseTerm(line);
+
+            if (line.TryPeek(out var op) && op.Operator == '+')
             {
                 line.Dequeue();
-                var secondTerm = ReadOneTerm(line, useOpPrecedence);
+                var secondTerm = Term(line);
                 return result + secondTerm;
             }
 
             return result;
         }
-
-        private static List<List<MathElement>> ConvertInput(string input)
-        {
-            return input.SplitIntoLines()
-                .Select(line => RegexUtils.DeserializeMany<MathElement>(line, @"(?<Number>\d+)|(?<Operator>[+*()])"))
-                .ToList();
-        }
-    }
-
-    public class MathElement
-    {
-        public long? Number { get; set; }
-        public char? Operator { get; set; }
     }
 }
